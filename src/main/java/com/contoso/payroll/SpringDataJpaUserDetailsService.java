b0e01c53d45c453beeaed79a7fdb32be8faa6f44
Rabi Achrafi <rabi.achrafi@gmail.com>
2024-04-05 11:24:41 +0100

    Remove reference to getPassword()

--- src/main/java/com/contoso/payroll/SpringDataJpaUserDetailsService.java
+++ src/main/java/com/contoso/payroll/SpringDataJpaUserDetailsService.java
@@ -21,8 +21,7 @@ public class SpringDataJpaUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
 		Manager manager = this.repository.findByName(name);
-		return new User(manager.getName(), manager.getPassword(),
-				AuthorityUtils.createAuthorityList(manager.getRoles()));
+		return new User(manager.getName(), AuthorityUtils.createAuthorityList(manager.getRoles()));
 	}
 
 }
